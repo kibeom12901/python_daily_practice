1b16b3d53a2f4ffd3107aabc856c8fc57aaf059c
Brian Kim <Kibeom.kim@tufts.edu>
2025-01-25 21:35:44 +0900

    Update main.py

--- snake_game/main.py
+++ snake_game/main.py
@@ -4,7 +4,7 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
-screen = Screen()
+screen = Screen() 
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
