1748cdd6354f1416a9652f7b284ecf2104271734
Brian Kim <Kibeom.kim@tufts.edu>
2025-02-03 15:25:00 +0900

    Update food.py

--- snake_game/food.py
+++ snake_game/food.py
@@ -12,7 +12,7 @@ class Food(Turtle):
         self.speed("fastest")
         self.refresh() 
 
-    def refresh(self):
+    def refresh(self): 
         random_x = random.randint(-280,280)
         random_y = random.randint(-280,280)
         self.goto(random_x,random_y)
