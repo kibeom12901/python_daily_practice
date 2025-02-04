a398e18ed9149919ba2bdfa32d94acb9d0c33c5e
Apple <apple@Appleui-MacBookPro.local>
2025-02-04 15:12:50 +0900

    Added U.S. States guessing game inside python_basics/us_states_game

---
+++ us_states_game/main.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import pandas
+
+screen = Screen()
+turtle = Turtle()
+
+screen.title("U.S States game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+screen.bgpic(image)
+guessed_states = []
+
+while len(guessed_states) < 50:
+    user_guess = screen.textinput(title = f"{len(guessed_states)}/50 states correct", prompt="Whats Your Guess").title()
+
+    data = pandas.read_csv("50_states.csv")
+    all_states = data["state"].to_list()
+
+    if user_guess == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("learn.csv")
+        break
+
+    if user_guess in all_states:
+        guessed_states.append(user_guess)
+        turtle.hideturtle()
+        turtle.penup()
+        state_data = data[data["state"] == user_guess]
+        turtle.goto(int(state_data.x), int(state_data.y))
+        turtle.write(user_guess)
+
+screen.mainloop()
+
+
