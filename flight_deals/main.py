61895e62f98d321cffe71d6def7f3a93debaa9d3
Brian Kim <Kibeom.kim@tufts.edu>
2025-02-18 15:43:06 +0900

    Create main.py

---
+++ flight_deals/main.py
@@ -0,0 +1,37 @@
+import time
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+
+data_manager = DataManager()
+sheet_data = data_manager.get_sheet_data()
+flight_search = FlightSearch()
+
+ORIGIN_CITY_IATA = "LON"
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_iata_code(row["city"])
+        time.sleep(2)  
+print("Updated sheet_data with IATA codes:")
+print(sheet_data)
+
+for row in sheet_data:
+    data_manager.update_sheet_data(row_id=row["id"], new_iata_code=row["iataCode"])
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']} ({destination['iataCode']})...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_months_from_today
+    )
+
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    time.sleep(2) 
